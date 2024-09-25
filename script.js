97acd79f4a7b8bd333c158f8f802a0481e9cb8dc
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-09-25 15:11:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Solicitar a nota do aluno
+
+let nota = parseFloat(prompt("Digite a nota"));
+
+// Verificar se o aluno foi aprovado ou reprovado
+
+if (nota >= 7) {
+    alert("Aprovado");
+} else {
+    alert("Reprovado");
+}
